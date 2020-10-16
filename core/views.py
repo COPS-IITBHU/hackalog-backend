4d7505415c41d41706c9190efec26447f9520314
shubhanshu02 <shubhanshu.e01@gmail.com>
2020-10-17 01:29:31 +0530

    Added Hackathon CRUD API Views
    The following changes are done in this commit
    1. Added Generic CreateAPIView for hackathon creation
    2. Added Generic RetrieveUpdateDestroyAPIView for accessing and modifying hackathons with their id
    3. Added Hackathon Serializer
    3. Ran `manage.py makemigrations` to remove date and max_score fields from hackathon

--- core/views.py
+++ core/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import generics
+from rest_framework import status
+from .serializers import HackathonSerializer
+from .models import Hackathon
 
-# Create your views here.
+class HackathonCreateView(generics.CreateAPIView):
+    '''
+    API used to create hackathon objects. Can only be accessed by the Super User
+    '''
+    serializer_class = HackathonSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class HackathonsRUDView(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    API used to read, update or delete the hackathon objects by their id. Only the Super User has the permissions to update or delete hackathon objects.
+    '''
+    
+    class HackathonPermissions(permissions.BasePermission):
+        '''
+        1. GET can be accessed even by anonymous users
+        2. PUT, PATCH and DELETE can only be accessed by the Super User
+        '''
+        def has_permission(self, request, view):
+            if (request.method in ['PUT','PATCH','DELETE']):
+                return bool(request.user and request.user.is_superuser)
+            elif (request.method == 'GET'):
+                return True
+            else:
+                return False
+    
+    permission_classes = [HackathonPermissions]
+    serializer_class = HackathonSerializer
+    lookup_url_kwarg = 'id'
+    queryset = Hackathon.objects.all()
