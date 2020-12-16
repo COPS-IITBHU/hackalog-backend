8de4c0148a78e5c16cb1a61eacf94245178ac3e8
Shubhanshu Saxena <shubhanshu.saxena2002@gmail.com>
2020-12-16 14:37:42 +0530

    Correct POST method for HackathonSubmissionView

--- core/serializers.py
+++ core/serializers.py
@@ -101,6 +101,12 @@ class SubmissionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = '__all__'
+
+    def validate_score(self, score):
+        if score < 0:
+            raise serializers.ValidationError("Score can't be less than 0")
+        return score
+        
 class MemberExitSerializer(serializers.Serializer):
 
     def exit_team(self):
