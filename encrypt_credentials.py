a78dcf337e5f6ae3025c3ee66b8fcf044067eab6
m-e-l-u-h-a-n <purushottam.tiwari.cd.cse19@itbhu.ac.in>
2021-05-22 18:45:39 +0530

    Add checks to prevent anyone from using template password.

--- encrypt_credentials.py
+++ encrypt_credentials.py
@@ -6,9 +6,12 @@ bufferSize = 64 * 1024
 password = "please-use-a-long-random-password"
 
 # encrypt
-with open("firebase_admin.json", "rb") as fIn:
-    with open("firebase_admin.aes", "wb") as fOut:
-        pyAesCrypt.encryptStream(fIn, fOut, password, bufferSize)
+if password != "please-use-a-long-random-password":
+    with open("firebase_admin.json", "rb") as fIn:
+        with open("firebase_admin.aes", "wb") as fOut:
+            pyAesCrypt.encryptStream(fIn, fOut, password, bufferSize)
+else:
+    raise ValueError("You cannot use template password, please use a long random password for encryption.\n\tYou are advised to edit the password(line: 6) before encrypting.")
 
 # get encrypted file size
 encFileSize = stat("firebase_admin.aes").st_size
